41c383d95c413187adff3ac242e18c7df054b292
Preston Wallace <wallace.preston@gmail.com>
2023-03-01 07:23:56 -0800

    react app in its own components dir

--- index.js
+++ index.js
@@ -1,27 +1,8 @@
 import React from "react"
 import { createRoot } from 'react-dom/client';
 
-import data from './data.json';
+import App from './components/App';
 
-function App() {
-  console.log('>>>>>>>>> data', data);
-  
-  return (
-    <div>
-      <h1>Invoice</h1>
-      {
-        data.map((item) => {
-          return (
-            <div>
-              <p>{item.Description}</p>
-            </div>
-          )
-        })
-      }
-
-    </div>
-  )
-}
 const root = createRoot(document.getElementById('root'));
 root.render(
   <div>
